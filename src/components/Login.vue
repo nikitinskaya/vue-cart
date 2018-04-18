07020e5fc0f2fac882a76c0801eacb818ce2c8ca
Vera Nikitinskaya <nikitinskaya.v@gmail.com>
2018-04-18 22:38:38 +0300

    Add login form

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,3 +1,17 @@
 <template>
-  <h1>Login</h1>
+  <div class="container mt-5">
+    <div class="row justify-content-center">
+      <form>
+        <div class="form-group">
+          <label for="email" class="col-form-label">Email</label>
+          <input type="text" class="form-control" id="email" placeholder="email@example.com">
+        </div>
+        <div class="form-group">
+          <label for="password" class="col-form-label">Password</label>
+          <input type="password" class="form-control" id="password" placeholder="Password">
+        </div>
+        <button type="submit" class="btn btn-primary">Login</button>
+      </form>
+    </div>
+  </div>
 </template>
