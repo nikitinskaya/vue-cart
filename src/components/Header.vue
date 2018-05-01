5b0493b17121855784bcf75a7bf2720564b887bc
Vera Nikitinskaya <nikitinskaya.v@gmail.com>
2018-05-01 23:15:48 +0300

    Change header link color

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,8 +5,8 @@
       <span class="navbar-text my-2 mx-2">Your balance: ${{ funds }}</span>
       <router-link to="topup" class="btn btn-outline-success my-2 mx-2">Top Up</router-link>
     </div>
-    <router-link class="nav-link" to="/login">Login</router-link>
-    <router-link class="nav-link" to="/">Logout</router-link>
+    <router-link class="nav-link text-info" to="/login">Login</router-link>
+    <router-link class="nav-link text-info" to="/">Logout</router-link>
   </nav>
 </template>
 
