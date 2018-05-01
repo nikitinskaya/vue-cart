ea506f1e8179e82e85d607b4d2b934c6460fcc17
Vera Nikitinskaya <nikitinskaya.v@gmail.com>
2018-05-01 19:47:50 +0300

    Add TopUp

--- src/components/TopUp.vue
+++ src/components/TopUp.vue
@@ -1,3 +1,11 @@
 <template>
-  <h1>Top Up</h1>
+  <div class="row no-gutters justify-content-center">
+    <form>
+      <div class="form-group">
+        <label for="amount" class="col-form-label">Amount</label>
+        <input type="number" class="form-control" id="amount">
+      </div>
+      <button type="submit" class="btn btn-primary">Add to balance</button>
+    </form>
+  </div>
 </template>
