3b81b4e9b25beafeb71ede9bc1f584ea0a859589
Vera Nikitinskaya <nikitinskaya.v@gmail.com>
2018-05-02 18:59:53 +0300

    Update data

--- src/data/goods.js
+++ src/data/goods.js
@@ -1,6 +1,6 @@
 export default [
-  { id: 1, name: 'One', price: '100'},
-  { id: 2, name: 'Two', price: '200'},
-  { id: 3, name: 'Three', price: '300'},
-  { id: 4, name: 'Four', price: '400'}
+  { id: 1, name: 'Donut', price: '100'},
+  { id: 2, name: 'Pickle Rick', price: '200'},
+  { id: 3, name: 'Tea', price: '300'},
+  { id: 4, name: 'Cheesecake', price: '400'}
 ]
