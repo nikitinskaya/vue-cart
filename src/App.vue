7c0a97686775cb622cde844e8d4b57187b62efea
Vera Nikitinskaya <nikitinskaya.v@gmail.com>
2018-05-11 22:47:30 +0300

    add animation on route switch

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
   <div class="container">
     <app-header></app-header>
     <div class="mt-5">
-      <router-view></router-view>
+      <transition name="slide" mode="out-in">
+        <router-view></router-view>
+      </transition>
     </div>
   </div>
 </template>
@@ -18,3 +20,35 @@ export default {
   }
 }
 </script>
+
+<style>
+  .slide-enter-active {
+    animation: slide-in 250ms ease-out forwards;
+  }
+
+  .slide-leave-active {
+    animation: slide-out 250ms ease-out forwards;
+  }
+
+  @keyframes slide-in {
+    from {
+      transform: translateY(-30px);
+      opacity: 0;
+    }
+    to {
+      transform: translateY(0);
+      opacity: 1;
+    }
+  }
+
+  @keyframes slide-out {
+    from {
+      transform: translateY(0);
+      opacity: 1;
+    }
+    to {
+      transform: translateY(-30px);
+      opacity: 0;
+    }
+  }
+</style>
